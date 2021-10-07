8397578b49736bef2a014de1a88b229ff7e2e3c5
Morrese Green Jr <greenmo57@gmail.com>
2021-10-06 23:56:07 -0400

    I was able to modify the orginal code you had by setting values with getElement by ID.  I still have not created the Id's in the html yet.

---
+++ script.js
@@ -0,0 +1,40 @@
+// Search Bar 
+// 1. Take user input
+// 2. Return data from API
+// 3. Console log it to check 
+// 4. Traverse data to populate/find temp, humitdity, wind, weather icon, future weather, days/date, highs and lows
+
+
+/**
+ * Fetch API
+ *  
+ * fetch();
+ */
+
+
+
+
+/**
+ * output javascript to html
+ * Create or use Array/information from API 
+ * build function that takes in one parameter
+ * log data using get element by id
+ * use a for loop to pass data
+ * set variables to data
+ * index data and grab the name
+ * set to Innerhtml
+ * 
+ * call/run using variable(array)
+ */
+
+/*function variable(data){
+    var name = document.getElementById(`name`)
+
+    // create for loop
+    for (var i = 0; < data.length; i++){
+        var name = ${data[i].name}//do this for every element
+
+        variablename.innerHTML += variablename
+    }
+}*/
+
