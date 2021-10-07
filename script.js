fb0fb0e3417f9d5053ba32cb9c1f0310a86cc959
Morrese Green Jr <greenmo57@gmail.com>
2021-10-07 09:46:30 -0400

    Updated version of jsavascript

--- script.js
+++ script.js
@@ -5,8 +5,10 @@
 // 4. Traverse data to populate/find temp, humidity, wind, weather icon, future weather, days/date, highs and lows
 // autocomplete?
 
+/*console.log("This is mo")
+
 const searchBtn = document.getElementById("searchBtn");
-const url = `https://api.openweathermap.org/data/2.5/weather?q=atlanta&appid=c1032cc72b1ef5fe78257a9931020ec3`;
+const url = `https://api.openweathermap.org/data/2.5/weather?q=atlanta&appid=${apiKey}`;
 const apiKey = "c1032cc72b1ef5fe78257a9931020ec3";
 
 let header = document.getElementById("header");
@@ -23,9 +25,16 @@ function getTemp() {
       let temp = data.main.temp;
       let humidity = data.main.humidity;
       let tempHigh = data.main.temp_max;
-      let tempLow = data.main.temp_low;
-      let cityName = data.main.name;
+      let tempLow = data.main.temp_min;
+      let cityName = data.name;
       let weather = data.weather[0].description;
+
+      console.log(temp);
+      console.log(humidity);
+      console.log(tempHigh);
+      console.log(tempLow);
+      console.log(cityName);
+      console.log(weather);
 
       //   console.log(weather);
 
@@ -53,4 +62,31 @@ function getTemp() {
         header.innerHTML = `<p> There are no results please try again </p>`;
       }
     });
-}
+}*/
+
+const submitBtn= document.getElementById("submit-btn")
+submitBtn.addEventListener('click',function(){
+  const inputbtn = document.getElementById('input-btn').value
+
+  fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputbtn+'&appid=c1032cc72b1ef5fe78257a9931020ec3')
+  .then (Response => Response.json())
+  .then(data =>{
+      let temp = data.main.temp;
+      let humidity = data.main.humidity;
+      let tempHigh = data.main.temp_max;
+      let tempLow = data.main.temp_min;
+      let cityName = data.name;
+      let weather = data.weather;
+
+    document.getElementById('city').innerText = cityName
+    document.getElementById('temp').innerText = temp
+    document.getElementById('temph').innerText = temp_max
+    document.getElementById('templ').innerText = temp_min
+    document.getElementById('humid').innerText = humidity
+    document.getElementById('weather').innerText = weather
+
+
+  })
+  .catch(res => alert("Type Correct City Name"))
+})
+
