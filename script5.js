d6d8c16bbdae379a4f0c97733dbd67e264b45877
Morrese Green Jr <greenmo57@gmail.com>
2021-10-07 10:00:33 -0400

    updated js code

---
+++ script5.js
@@ -0,0 +1,25 @@
+const submitBtn= document.getElementById("submit-btn")
+submitBtn.addEventListener('click',function(){
+  const inputbtn = document.getElementById('input-btn').value
+
+  fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputbtn+'&appid=c1032cc72b1ef5fe78257a9931020ec3')
+  .then (Response => Response.json())
+  .then(data =>{
+      let temp = data.main.temp;
+      let humidity = data.main.humidity;
+      let tempHigh = data.main.temp_max;
+      let tempLow = data.main.temp_min;
+      let cityName = data.name;
+      let weather = data.weather;
+
+    document.getElementById('city').innerText = cityName
+    document.getElementById('temp').innerText = temp
+    document.getElementById('temph').innerText = temp_max
+    document.getElementById('templ').innerText = temp_min
+    document.getElementById('humid').innerText = humidity
+    document.getElementById('weather').innerText = weather
+
+
+  })
+  .catch(res => alert("Type Correct City Name"))
+})
