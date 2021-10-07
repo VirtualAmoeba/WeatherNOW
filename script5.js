3bbd81b87d83c54d185197c3695e252dc3fc7df7
Morrese Green Jr <greenmo57@gmail.com>
2021-10-07 10:56:14 -0400

    had to fix some errors in the updated code

--- script5.js
+++ script5.js
@@ -1,25 +1,33 @@
-const submitBtn= document.getElementById("submit-btn")
-submitBtn.addEventListener('click',function(){
-  const inputbtn = document.getElementById('input-btn').value
+const submitBtn= document.getElementById("submit-btn");
+const inputBtn = document.getElementById('cityInput');
 
-  fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputbtn+'&appid=c1032cc72b1ef5fe78257a9931020ec3')
-  .then (Response => Response.json())
-  .then(data =>{
-      let temp = data.main.temp;
-      let humidity = data.main.humidity;
-      let tempHigh = data.main.temp_max;
-      let tempLow = data.main.temp_min;
-      let cityName = data.name;
-      let weather = data.weather;
+const getWeatherData = (cityName) => {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid=c1032cc72b1ef5fe78257a9931020ec3')
+    .then (Response => console.log(Response.json()))
+    .then(data =>{
+        let temp = data.main.temp;
+        let humidity = data.main.humidity;
+        let tempHigh = data.main.temp_max;
+        let tempLow = data.main.temp_min;
+        let cityName = data.name;
+        let weather = data.weather;
+        
+        document.getElementById('city').innerText = cityName
+        document.getElementById('temp').innerText = temp
+        document.getElementById('temph').innerText = temp_max
+        document.getElementById('templ').innerText = temp_min
+        document.getElementById('humid').innerText = humidity
+        document.getElementById('weather').innerText = weather
+        
+        
+    })
+    .catch(res => alert("Type Correct City Name"))
+}
 
-    document.getElementById('city').innerText = cityName
-    document.getElementById('temp').innerText = temp
-    document.getElementById('temph').innerText = temp_max
-    document.getElementById('templ').innerText = temp_min
-    document.getElementById('humid').innerText = humidity
-    document.getElementById('weather').innerText = weather
+submitBtn.addEventListener('click', e => {
+    
+    
+    e.preventDefault();
+    getWeatherData(inputBtn.value);
+});
 
-
-  })
-  .catch(res => alert("Type Correct City Name"))
-})
